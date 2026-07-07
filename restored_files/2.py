6578d515ac719846c0f0e4d7a1fc12aa412037fc
ljb <2401551879@11.com>
2026-07-07 21:56:54 +0800

    ljb 提交

--- restored_files/2.py
+++ restored_files/2.py
@@ -1,2 +1,6 @@
 print("hellogsajgdkhaldlfl world1234567")
 print("hellogsajgdkhaldlfl world1234567")
+print("hellogsajgdkhaldlfl world1234567")
+print("hellogsajgdkhaldlfl world1234567")
+print("hellogsajgdkhaldlfl world1234567")
+print("hellogsajgdkhaldlfl world1234567")
